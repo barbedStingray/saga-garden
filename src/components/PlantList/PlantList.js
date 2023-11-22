37969a1e053fc049a75cf23c8b4e99ae4a8988c3
Ben Rehmann <brehmann@me.com>
2023-11-22 13:19:35 -0600

    updating form

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -6,15 +6,29 @@ function PlantList() {
     const dispatch = useDispatch();
 
     const reduxState = useSelector(store => store);
+    const plantList = useSelector(store => store.plantList);
+
+    const getPlantList = () => {
+
+        dispatch({ type: 'FETCH_PLANT_LIST' });
+    }
+
 
     useEffect(() => {
         console.log('component did mount');
         // dispatch an action to request the plantList from the API
+        getPlantList();
     }, []); 
 
     return (
         <div>
             <h3>This is the plant list</h3>
+            <ul>
+                {plantList.map((plant) => 
+                <li key={plant.id}>{plant.name}</li>)}
+            </ul>
+
+
             <pre>{JSON.stringify(reduxState)}</pre>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,29 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+
+// imports
+import logger from 'redux-logger';
+
+// SAGA step 1 - imports
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+
 
 import App from './App';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
 
-const plantList = (state = startingPlantArray, action) => {
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+      return [ ...state, action.payload ];
+    case 'SET_PLANT_LIST':
+      return action.payload;
     default:
       return state;
   }
 };
 
+
+// const plantOriginalList = (state = [], action) => {
+//     if(action.type === 'SET_PLANT_LIST') {
+//       return action.payload;
+//     }
+//     return state;
+// }
+
+
+function* fetchPlantList() {
+  try {
+
+    const response = yield axios.get('/api/plant');
+
+    const action = { type: 'SET_PLANT_LIST', payload: response.data }
+
+    yield put(action);
+
+  } catch (error) {
+    console.log(`error GET /api/plant`, error);
+    alert(`something went wrong`);
+  }
+}
+
+
+
+function* rootSaga() {
+  // all your sagas here
+  yield takeEvery('FETCH_PLANT_LIST', fetchPlantList);
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({ 
+    plantList 
+  }),
+  applyMiddleware(sagaMiddleware, logger),
 );
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
